205c81a960d03f5ee5931746481b980109df3462
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-17 13:47:28 +0900

    test: 글 수정 관련 테스트 코드 추가

---
+++ src/test/java/com/study/model/board/ModDateTest.java
@@ -0,0 +1,15 @@
+package com.study.model.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+@DisplayName("글 수정일자는")
+public class ModDateTest {
+
+    @Test
+    void create() {
+        ModDate modDate = new ModDate(LocalDateTime.now());
+    }
+}
