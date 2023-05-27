a0a226de9e67fad77e8e1aea0b21b62493f45dcf
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-27 14:05:50 +0900

    feat: Http Metthod에 따른 메서드 호출 로직 추가

--- src/main/java/com/study/core/mvc/AbstractController.java
+++ src/main/java/com/study/core/mvc/AbstractController.java
@@ -1,24 +1,28 @@
 package com.study.core.mvc;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public abstract class AbstractController implements Controller {
+@Slf4j
+public class AbstractController implements Controller {
 
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) {};
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {};
 
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) {};
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {};
 
     public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getMethod() == "GET") {
+        String httpMethod = req.getMethod();
+        log.debug("request...Http Method : {}", httpMethod);
+
+        if(httpMethod.equals("GET")) {
             doGet(req, resp);
         }
-
-        if (req.getMethod() == "POST") {
+        if(httpMethod.equals("POST")) {
             doPost(req, resp);
         }
     }
-
 }
