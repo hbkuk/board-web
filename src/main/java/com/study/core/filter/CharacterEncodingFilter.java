713521c81d9e610ce1a3ee522101ec4a5bf4f803
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-30 01:49:01 +0900

    docs: 서블릿 필터 javadoc 추가

--- src/main/java/com/study/core/filter/CharacterEncodingFilter.java
+++ src/main/java/com/study/core/filter/CharacterEncodingFilter.java
@@ -4,6 +4,9 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+/**
+ * 요청과 응답의 문자를 인코딩을 UTF-8로 설정하는 서블릿 필터
+ */
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
     private static final String DEFAULT_ENCODING = "UTF-8";
@@ -12,6 +15,13 @@ public class CharacterEncodingFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    /**
+     * 요청과 응답을 처리하여 문자 인코딩을 UTF-8로 설정하고 요청과 응답을 전달합니다
+     *
+     * @param request  서블릿 요청 객체
+     * @param response 서블릿 응답 객체
+     * @param chain    필터 체인 객체
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
--- src/main/java/com/study/core/filter/ResourceFilter.java
+++ src/main/java/com/study/core/filter/ResourceFilter.java
@@ -1,7 +1,6 @@
 package com.study.core.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -10,9 +9,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ *  정적 리소스(css, js 등) 요청을 처리하기 위한 필터입니다.
+ */
+@Slf4j
 @WebFilter("/*")
 public class ResourceFilter implements Filter {
-    private static final Logger logger = LoggerFactory.getLogger(ResourceFilter.class);
     private static final List<String> resourcePrefixes = new ArrayList<>();
     static {
         resourcePrefixes.add("/resources/css");
@@ -24,24 +27,43 @@ public class ResourceFilter implements Filter {
 
     private RequestDispatcher defaultRequestDispatcher;
 
+    /**
+     * 필터를 초기화합니다.
+     *
+     * @param filterConfig 필터 구성 객체
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         this.defaultRequestDispatcher = filterConfig.getServletContext().getNamedDispatcher("default");
     }
 
+    /**
+     * 요청과 응답을 처리하여 리소스 요청인 경우 다른 서블릿이나 리소스로 전달하고,
+     * 그렇지 않은 경우 필터 체인을 따라 다음 필터로 전달합니다.
+     *
+     * @param request  서블릿 요청 객체
+     * @param response 서블릿 응답 객체
+     * @param chain    필터 체인 객체
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         String path = req.getRequestURI().substring(req.getContextPath().length());
         if (isResourceUrl(path)) {
-            logger.debug("path : {}", path);
+            log.debug("path : {}", path);
             defaultRequestDispatcher.forward(request, response);
         } else {
             chain.doFilter(request, response);
         }
     }
 
+    /**
+     * 주어진 URL이 리소스 URL인지 여부를 확인합니다.
+     *
+     * @param url 확인할 URL
+     * @return URL이 리소스 URL인 경우 true, 그렇지 않은 경우 false
+     */
     private boolean isResourceUrl(String url) {
         for (String prefix : resourcePrefixes) {
             if (url.startsWith(prefix)) {
